d9152378c61e4b60fb923c629687b22e2cce3b8c
hsw0905 <hsw0905@gmail.com>
2020-12-02 18:14:27 +0900

    feat game

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class User {
+    private Scanner sc = new Scanner(System.in);
+
+    private InputValidator inputValidator;
+
+    public User() {
+        inputValidator = new InputValidator();
+    }
+
+    public String inputNumber() {
+        return inputValidator.isValidString(InputView.getUserString());
+    }
+}
