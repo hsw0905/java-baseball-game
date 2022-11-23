5bb5fb4a1b177af2de72ac6233973dc21a63dab7
hsw0905 <hsw0905@gmail.com>
2022-11-23 18:28:43 +0900

    불필요 코드 정리

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,15 +1,12 @@
 package baseball;
 
 public class GameController {
-  private final String WRONG_INPUT_EXCEPTION_MESSAGE = "잘못된 입력값입니다. 게임이 종료되었습니다.";
 
   private final InputConsole inputConsole;
   private final OutputConsole outputConsole;
   private final Judgement judgement;
   private final InputValidator inputValidator;
   private final RandomBall randomBall;
-  private Ball userBall;
-  private Ball computerBall;
 
   public GameController(InputConsole inputConsole, OutputConsole outputConsole, Judgement judgement,
       InputValidator inputValidator,
@@ -23,13 +20,13 @@ public class GameController {
 
   public void run() {
     String userInputNumber = "";
-    computerBall = randomBall.create();
+    Ball computerBall = randomBall.create();
 
-    while (judgement.getStrike() != 3) {
+    while (judgement.getStrike() != GameRule.DIGIT_RANGE.getNum()) {
       judgement.reset();
       inputConsole.printInputBall();
       userInputNumber = inputConsole.requestNumber();
-      userBall = inputValidator.toBall(userInputNumber);
+      Ball userBall = inputValidator.toBall(userInputNumber);
 
       judgement.judge(userBall, computerBall);
       outputConsole.printResult(judgement.getStrike(), judgement.getBall());
