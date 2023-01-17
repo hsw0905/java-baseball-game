efbf4abb6492bb72424e9cd8b5123a62c029649c
hsw0905 <hsw0905@gmail.com>
2023-01-17 15:41:22 +0900

    code reset, 다시 구현하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,11 +6,5 @@ public class Application {
 
   public static void main(String[] args){
     //TODO: 숫자 야구 게임 구현
-    GameController controller = new GameController(
-        new InputConsole(), new OutputConsole(), new Judgement(), new InputValidator(),
-        new RandomBall()
-    );
-
-      controller.run();
   }
 }
